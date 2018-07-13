ebc3a0f8435b2ae797d2e5f1069aa6df1b1c3f84
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-13 18:25:55 +0300

    add driver

--- src/com/buildinglink/mainapp/debug/qa/FrontDeskInstructionsScreen.java
+++ src/com/buildinglink/mainapp/debug/qa/FrontDeskInstructionsScreen.java
@@ -1,4 +1,12 @@
 package com.buildinglink.mainapp.debug.qa;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
 public class FrontDeskInstructionsScreen {
+    private static AppiumDriver<MobileElement> driver;
+
+    public FrontDeskInstructionsScreen(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+    }
 }
--- src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
+++ src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
@@ -1,4 +1,12 @@
 package com.buildinglink.mainapp.debug.qa;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
 public class RepairRequestsScreen {
+    private static AppiumDriver<MobileElement> driver;
+
+    public RepairRequestsScreen(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+    }
 }
