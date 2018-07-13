e64660b32e7cd2af3785fc69d62127dd528bfac1
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-13 18:25:03 +0300

    update HomeScreen class

--- src/com/buildinglink/mainapp/debug/qa/HomeScreen.java
+++ src/com/buildinglink/mainapp/debug/qa/HomeScreen.java
@@ -27,4 +27,57 @@ public class HomeScreen {
     public HomeScreen(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
     }
+
+    private By allowPushNotificationsAllert = By.id("android:id/message");
+    private By refreshButton = By.id("com.buildinglink.mainapp.debug.qa:id/action_refresh");
+    private By okButton = By.id("android:id/button1");
+    private By cancelButton = By.id("android:id/button2");
+    private By allUpcomingEvents = By.id("com.buildinglink.mainapp:id/agendaDescriptionView");
+    private By homeIcon = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.TextView[1]");
+    private By lifestyleIcon = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.TextView[2]");
+    private By greenPlusButton = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.ImageButton");
+    private By contactsIcon = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.TextView[3]");
+    private By myProfileIcon = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.TextView[4]");
+    private By repairRequestsButton = By.name("Repair Requests");
+
+    public HomeScreen tapOnOkButton(){
+        WebDriverWait wait = new WebDriverWait(driver,120);
+        wait.until(ExpectedConditions.elementToBeClickable(okButton));
+        driver.findElement(okButton).click();
+        return this;
+    }
+
+    public HomeScreen tapOnCancelButton(){
+        WebDriverWait wait = new WebDriverWait(driver,120);
+        wait.until(ExpectedConditions.elementToBeClickable(cancelButton));
+        driver.findElement(cancelButton).click();
+        return this;
+    }
+
+    public HomeScreen tapRefreshButton(){
+        driver.findElement(refreshButton).click();
+        return this;
+    }
+
+    public HomeScreen tapHomeIcon(){
+        driver.findElement(homeIcon).click();
+        return this;
+    }
+
+    public HomeScreen tapRepairRequestsButton(){
+        driver.findElement(repairRequestsButton).click();
+        return this;
+    }
+
+    public RepairRequestsScreen openRepairRequestsModule (){
+        this.tapRepairRequestsButton();
+        return new RepairRequestsScreen(driver);
+    }
+
+
+
+
+
+
+
 }
