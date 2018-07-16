512d990982298a7ad7805e24a4d485a0325f918b
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-16 15:47:21 +0300

    add variables

--- src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
+++ src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
@@ -2,11 +2,20 @@ package com.buildinglink.mainapp.debug.qa;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+import java.util.List;
+import java.util.Random;
 
 public class RepairRequestsScreen {
     private static AppiumDriver<MobileElement> driver;
-
     public RepairRequestsScreen(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
     }
+
+    private By backButton = By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]");
+    private By title = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView");
+    private By addButton = By.id("com.buildinglink.mainapp.debug.qa:id/menu_item_add");
+    private By allRequests = By.className("android.widget.TextView");
+
 }
