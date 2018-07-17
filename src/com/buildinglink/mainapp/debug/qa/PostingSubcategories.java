abf8051d0b256cc650ad54a6758b8838211a2ec0
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-17 18:08:15 +0300

    add selectSubcategory()

--- src/com/buildinglink/mainapp/debug/qa/PostingSubcategories.java
+++ src/com/buildinglink/mainapp/debug/qa/PostingSubcategories.java
@@ -2,11 +2,24 @@ package com.buildinglink.mainapp.debug.qa;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+import java.util.Random;
 
 public class PostingSubcategories {
     private static AppiumDriver<MobileElement> driver;
 
     public PostingSubcategories(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
+    }
+
+    private By subcategoriesList = By.id("com.buildinglink.mainapp.debug.qa:id/bulletinBoardCategory");
+
+    public NewPosting selectSubcategory(){
+        int countAllSubcategories = driver.findElements(subcategoriesList).size();
+        Random random = new Random();
+        int getRandomCategory = random.nextInt(countAllSubcategories);
+        driver.findElements(subcategoriesList).get(getRandomCategory).click();
+        return new NewPosting(driver);
     }
 }
