a47d5b1c7e9dc3d54b5bc27b181a4d76c14d7ea8
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-19 18:41:39 +0300

    add new methods to work with screen

--- src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
+++ src/com/buildinglink/mainapp/debug/qa/RepairRequestsScreen.java
@@ -1,6 +1,8 @@
 package com.buildinglink.mainapp.debug.qa;
 
+import com.buildinglink.mainapp.prodBuild.RepairRequests;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
 
@@ -17,5 +19,27 @@ public class RepairRequestsScreen {
     private By title = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView");
     private By addButton = By.id("com.buildinglink.mainapp.debug.qa:id/menu_item_add");
     private By allRequests = By.className("android.widget.TextView");
+    private By requestDescription = MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().className(\"android.support.v7.widget.RecyclerView\")).scrollIntoView("
+            + "new UiSelector().resourceId(\"com.buildinglink.mainapp.debug.qa:id/description\"))");
+    private By editButton = By.id("com.buildinglink.mainapp.debug.qa:id/editButton");
+    private By activityLogButton = By.id("com.buildinglink.mainapp.debug.qa:id/activityLogText");
+
+    public NewRepairRequest tapAddButton(){
+        driver.findElement(addButton).click();
+        return new NewRepairRequest(driver);
+    }
+
+    public RepairRequestsScreen expandRequest(){
+        int allRequestsOnScreen = driver.findElements(requestDescription).size();
+        Random random = new Random();
+        int getRandomNumber = random.nextInt(allRequestsOnScreen);
+        driver.findElements(requestDescription).get(getRandomNumber).click();
+        return this;
+    }
+
+    public EditRepairRequest tapEditButton(){
+        driver.findElement(editButton).click();
+        return new EditRepairRequest(driver);
+    }
 
 }
