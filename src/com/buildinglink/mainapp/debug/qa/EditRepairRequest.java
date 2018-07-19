02439e8e386b402cea194e2c2353cbce4ed5f43e
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-07-19 19:19:47 +0300

    update methods

--- src/com/buildinglink/mainapp/debug/qa/EditRepairRequest.java
+++ src/com/buildinglink/mainapp/debug/qa/EditRepairRequest.java
@@ -14,33 +14,26 @@ public class EditRepairRequest extends NewRepairRequest {
 
     private By statusField = By.id("com.buildinglink.mainapp.debug.qa:id/status");
     private By openStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
-    private By onHoldStatus = By.name("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
-    private By closedStatus = By.name("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[3]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
+    private By onHoldStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
+    private By closedStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[3]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
     private By statusCheckedIndicator = By.id("com.buildinglink.mainapp.debug.qa:id/checkedIndicator");
     private By requestCategory = By.id(("com.buildinglink.mainapp.debug.qa:id/category"));
 
-    public EditRepairRequest changeStatus(){
-        //this.getCurrentStatus();
-        //this.selectRandomStatus();
-        return this;
-    }
-
-    public void tapStatusfield() {
+    public EditRepairRequest changeStatusfield() {
         this.getCurrentStatusInt();
-        System.out.println("CurrentStatusInt "+this.getCurrentStatusInt());
-        int statusInt = this.getRandomStatus();
+        int statusIntToChange = this.getRandomStatusInt();
         driver.findElement(statusField).click();
-        System.out.println("statusInt "+statusInt);
-        switch(statusInt){
+        switch(statusIntToChange){
             case 0: driver.findElement(openStatus).click();
                 break;
             case 1: driver.findElement(onHoldStatus).click();
                 break;
             case 2: driver.findElement(closedStatus).click();
         }
+        return this;
     }
 
-    public int getCurrentStatusInt(){
+    private int getCurrentStatusInt(){
         String currentStatus = driver.findElement(statusField).getText();
         int currentStatusInt = 0;
         switch(currentStatus){
@@ -53,7 +46,7 @@ public class EditRepairRequest extends NewRepairRequest {
         return currentStatusInt;
     }
 
-    private int getRandomStatus(){
+    private int getRandomStatusInt(){
         Random random = new Random();
         int randomStatusInt = random.nextInt(2);
         if (getCurrentStatusInt()==randomStatusInt){
@@ -67,7 +60,6 @@ public class EditRepairRequest extends NewRepairRequest {
                 randomStatusInt = 0;
             }
         }
-        System.out.println("randomStatusInt "+randomStatusInt);
         return randomStatusInt;
     }
 
