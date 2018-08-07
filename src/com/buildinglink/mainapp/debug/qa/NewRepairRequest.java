cee25c36cf9b904b4fc265ac1d6b1c4c821dc9ac
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-08-07 18:26:21 +0300

    add override methods

--- src/com/buildinglink/mainapp/debug/qa/NewRepairRequest.java
+++ src/com/buildinglink/mainapp/debug/qa/NewRepairRequest.java
@@ -16,7 +16,9 @@ public class NewRepairRequest {
     protected By backButton = By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]");
     protected By saveButton = By.id("com.buildinglink.mainapp.debug.qa:id/menu_save");
     protected By problemDescriptionField = By.id("com.buildinglink.mainapp.debug.qa:id/requestDescription");
-    protected By entryInstructionsField = By.id("com.buildinglink.mainapp.debug.qa:id/entryInstructions");
+    //protected By entryInstructionsField = By.id("com.buildinglink.mainapp.debug.qa:id/entryInstructions");
+    protected By entryInstructionsField = MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().className(\"android.view.ViewGroup\")).scrollIntoView("
+            + "new UiSelector().resourceId(\"com.buildinglink.mainapp.debug.qa:id/entryInstructions\"))");
     protected By contactPhoneField = MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().className(\"android.view.ViewGroup\")).scrollIntoView("
             + "new UiSelector().resourceId(\"com.buildinglink.mainapp.debug.qa:id/phone\"))");
     protected By additionalEmailField = By.id("com.buildinglink.mainapp.debug.qa:id/email");
@@ -48,24 +50,21 @@ public class NewRepairRequest {
         driver.navigate().back();
     }
 
-    public NewRepairRequest typeEntryInstructions(String entryInstructions) {
+    public void typeEntryInstructions(String entryInstructions) {
         driver.findElement(entryInstructionsField).sendKeys(entryInstructions);
         driver.navigate().back();
-        return this;
     }
 
-    public NewRepairRequest typeContactPhone(String contactPhone) {
+    public void typeContactPhone(String contactPhone) {
         driver.findElement(contactPhoneField).clear();
         driver.findElement(contactPhoneField).sendKeys(contactPhone);
         driver.navigate().back();
-        return this;
     }
 
-    public NewRepairRequest typeAdditionalEmail(String additionalEmail) {
+    public void typeAdditionalEmail(String additionalEmail) {
         driver.findElement(additionalEmailField).clear();
         driver.findElement(additionalEmailField).sendKeys(additionalEmail);
         driver.navigate().back();
-        return this;
     }
 
     public String getSuccessMessage(){
