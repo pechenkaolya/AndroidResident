2e841e493f0e2848ebf38fca906cca1c54acb3f0
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-08-22 19:29:01 +0300

    refactoring

--- src/com/buildinglink/mainapp/common/Server.java
+++ src/com/buildinglink/mainapp/common/Server.java
@@ -1,4 +1,12 @@
 package com.buildinglink.mainapp.common;
 
 public class Server {
+
+    public static String returnServer(){
+        String qaServer = "com.buildinglink.mainapp.debug.qa:";
+        String stagingServer = "com.buildinglink.mainapp.staging:";
+        String prodServer =  "com.buildinglink.mainapp:";
+        return qaServer;
+    }
+
 }
