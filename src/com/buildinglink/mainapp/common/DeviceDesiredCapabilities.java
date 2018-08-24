162b6b547b3e579b78bd59b0563bcfcb21b64643
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-08-24 18:27:25 +0300

    update android version for the Nexus device

--- src/com/buildinglink/mainapp/common/DeviceDesiredCapabilities.java
+++ src/com/buildinglink/mainapp/common/DeviceDesiredCapabilities.java
@@ -8,7 +8,7 @@ public class DeviceDesiredCapabilities {
     public static DesiredCapabilities setUpNexus5X(){
         DesiredCapabilities caps = new	DesiredCapabilities();//To	create	an	object
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "00f1edb5378094e3"); //Android-057
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0.0"); //Android-057
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0"); //Android-057
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.buildinglink.mainapp.debug.qa"); //package of the qa build
         caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.buildinglink.mainapp.login.view.viewcontrollers.activities.SplashActivity");//To specify the	activity which we want to launch
