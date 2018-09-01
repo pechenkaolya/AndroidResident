761e578626ba6a12d437cfed45d689ece73d1016
Olya <olya.suvalova@gmail.com>
2018-09-01 22:58:18 +0300

    refactoring

--- src/com/buildinglink/mainapp/debug/qa/EditRepairRequest.java
+++ src/com/buildinglink/mainapp/debug/qa/EditRepairRequest.java
@@ -13,9 +13,9 @@ public class EditRepairRequest extends NewRepairRequest {
     }
 
     private By statusField = By.id(Server.setEndpoint()+":id/status");
-    private By openStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
-    private By onHoldStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
-    private By closedStatus = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[3]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.TextView");
+    private By openStatus = By.xpath("//android.widget.TextView[@text=\"Open\"]");
+    private By onHoldStatus = By.xpath("//android.widget.TextView[@text=\"On Hold\"]");
+    private By closedStatus = By.xpath("//android.widget.TextView[@text=\"Closed\"]");
     private By statusCheckedIndicator = By.id(Server.setEndpoint() + ":id/checkedIndicator");
     private By requestCategory = By.id((Server.setEndpoint() + ":id/category"));
 
