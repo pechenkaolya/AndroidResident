806d14709c813fc314e3eb144b9876a569444944
Olya <olya.suvalova@gmail.com>
2018-09-02 00:15:25 +0300

    refactoring

--- src/com/buildinglink/mainapp/common/DeviceDesiredCapabilities.java
+++ src/com/buildinglink/mainapp/common/DeviceDesiredCapabilities.java
@@ -5,23 +5,27 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class DeviceDesiredCapabilities {
-    public static DesiredCapabilities setUpNexus5X(){
-        DesiredCapabilities caps = new	DesiredCapabilities();//To	create	an	object
+    private static DesiredCapabilities setUpNexus5X(){
+        DesiredCapabilities caps = new	DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "00f1edb5378094e3"); //Android-057
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0"); //Android-057
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,Server.setEndpoint());
         caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.buildinglink.mainapp.login.view.viewcontrollers.activities.SplashActivity");//To specify the	activity which we want to launch
         return caps;
     }
 
-    public static DesiredCapabilities setUpEmulator(){
-        DesiredCapabilities caps = new	DesiredCapabilities();//To	create	an	object
+    private static DesiredCapabilities setUpEmulator(){
+        DesiredCapabilities caps = new	DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0.0");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,Server.setEndpoint());
         caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.buildinglink.mainapp.login.view.viewcontrollers.activities.SplashActivity");//To specify the	activity which we want to launch
         return caps;
+    }
+
+    public static DesiredCapabilities setUpDevice(){
+        return DeviceDesiredCapabilities.setUpEmulator();
     }
 }
