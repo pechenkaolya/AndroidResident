972d49b525d5219140fe2fb8ef1f791a394b3b45
Olya <olya.suvalova@gmail.com>
2018-09-02 00:17:58 +0300

    refactoring

--- src/test/RepairRequestsTest.java
+++ src/test/RepairRequestsTest.java
@@ -25,7 +25,7 @@ public class RepairRequestsTest {
     @BeforeClass
     public static void setUp() {
         try {
-            driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), DeviceDesiredCapabilities.setUpNexus5X());
+            driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), DeviceDesiredCapabilities.setUpDevice());
 
             LoginScreen loginScreen = new LoginScreen(driver);
             loginScreen.loginWithTestUser();
