afcc21e1303b70f3577931509fd3cf83227e0d08
Olya <olya.suvalova@gmail.com>
2018-09-01 18:30:47 +0300

    refactoring

--- src/com/buildinglink/mainapp/common/Server.java
+++ src/com/buildinglink/mainapp/common/Server.java
@@ -2,7 +2,7 @@ package com.buildinglink.mainapp.common;
 
 public class Server {
 
-    public static String setUpEndpoint(){
+    public static String setEndpoint(){
         String qaServer = "com.buildinglink.mainapp.debug.qa:";
         String stagingServer = "com.buildinglink.mainapp.debug.staging:";
         String prodServer =  "com.buildinglink.mainapp:";
