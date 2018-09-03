7e0d6823271bb9adb1566ffa77506fc1ba5c8ad1
ICX\Volha.Suvalava <olya.suvalova@gmail.com>
2018-09-03 12:05:22 +0300

    delete unnecessary packages

--- src/com/buildinglink/mainapp/common/Server.java
+++ src/com/buildinglink/mainapp/common/Server.java
@@ -3,9 +3,9 @@ package com.buildinglink.mainapp.common;
 public class Server {
 
     public static String setEndpoint(){
-        String qaServer = "com.buildinglink.mainapp.debug.qa";
-        String stagingServer = "com.buildinglink.mainapp.debug.staging";
-        String prodServer =  "com.buildinglink.mainapp";
+        String qaServer = "com.buildinglink.mainapp.debug.qa:";
+        String stagingServer = "com.buildinglink.mainapp.debug.staging:";
+        String prodServer =  "com.buildinglink.mainapp:";
         return qaServer;
     }
 
